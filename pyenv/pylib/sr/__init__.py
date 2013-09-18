8e0fc2751e691ca5388aa54eed98da4b25d862f8
Alastair Lynn <arplynn@gmail.com>
2013-09-18 12:54:45 +0100

    Export constants from the ruggeduino module

--- pyenv/pylib/sr/__init__.py
+++ pyenv/pylib/sr/__init__.py
@@ -2,4 +2,5 @@ from robot import Robot, NoCameraPresent
 from event import wait_for
 from poll import And, Or, Poll
 from vision import MARKER_ARENA, MARKER_ROBOT, MARKER_PEDESTAL, MARKER_TOKEN
+from ruggeduino import HIGH, LOW, INPUT, OUTPUT, INPUT_PULLUP
 
